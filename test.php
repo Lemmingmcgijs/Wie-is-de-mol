83291547263724901cf5fa7f124bafcaff1eb3e6
Lemmingmcgijs <160509289+Lemmingmcgijs@users.noreply.github.com>
2026-05-09 13:19:50 +0000

    verder met de test

--- test.php
+++ test.php
@@ -4,12 +4,25 @@
         include("head.php");
     ?>
     <body>
-        <img src="assets/logo.jpg">
         <div class="container">
-            <h2>20. Wie is de mol?</h2>
-            <form>
-                <input type="checkbox">
-            </form>
+            <a href="index.php"><img src="assets/logo.jpg"></a>
+            <h1>20. Wie is de mol?</h1>
         </div>
+
+        <form>
+            <div class="container">
+                <input type="radio" id="ans_1" name="ans_1">
+                <label for="ans_1">Test1</label>
+            </div>
+            <div class="container">
+                <input type="radio" id="ans_2" name="ans_1">
+                <label for="ans_2">Test2</label>
+            </div>
+            <div class="container">
+                <input type="radio" id="ans_3" name="ans_1">
+                <label for="ans_3">Test3</label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     </body>
 </html>

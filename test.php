25c88c2703722d0d96a2e10ed5996de440dc59f1
Lemmingmcgijs <160509289+Lemmingmcgijs@users.noreply.github.com>
2026-05-09 14:19:30 +0000

    nog verder met de test

--- test.php
+++ test.php
@@ -4,22 +4,23 @@
         include("head.php");
     ?>
     <body>
-        <div class="container">
+        <header>
             <a href="index.php"><img src="assets/logo.jpg"></a>
             <h1>20. Wie is de mol?</h1>
-        </div>
+            <h2>Kandidaat: <?php echo $_POST["code"];?></h2>
+        </header>
 
-        <form>
-            <div class="container">
-                <input type="radio" id="ans_1" name="ans_1">
+        <form method="post" action="verwerk.php">
+            <div class="antwoord">
+                <input type="radio" id="ans_1" name="ans" value="1">
                 <label for="ans_1">Test1</label>
             </div>
-            <div class="container">
-                <input type="radio" id="ans_2" name="ans_1">
+            <div class="antwoord">
+                <input type="radio" id="ans_2" name="ans" value="2">
                 <label for="ans_2">Test2</label>
             </div>
-            <div class="container">
-                <input type="radio" id="ans_3" name="ans_1">
+            <div class="antwoord">
+                <input type="radio" id="ans_3" name="ans" value="3">
                 <label for="ans_3">Test3</label>
             </div>
             <button type="submit">Submit</button>
---
+++ verwerk.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html class="index">
+    <?php
+        include("head.php");
+    ?>
+    <body>
+        <h1><?php echo $_POST["ans"]; ?></h1>
+    </body>
+</html>

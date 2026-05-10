fda29423b585fc986b65d038753a94fb48d38996
Lemmingmcgijs <gijs@famjolink.nl>
2026-05-10 20:59:54 +0200

    super upgrade voor de test

--- test.php
+++ test.php
@@ -4,13 +4,11 @@
         include("head.php");
     ?>
     <body>
-        <header>
-            <a href="index.php"><img src="assets/logo.jpg"></a>
-            <h1>20. Wie is de mol?</h1>
-            <h2>Kandidaat: <?php echo $_POST["code"];?></h2>
-        </header>
-
+        <div class="logo"><a href="index.php"><img src="assets/logo.jpg"></a></div>
+        
         <div class="blok">
+            <h1>20. Wie is de mol?</h1>
+
             <form method="post" action="verwerk.php">
                 <ul>
                     <li class="antwoord">
@@ -25,9 +23,24 @@
                         <input type="radio" id="ans_3" name="ans" value="3">
                         <label for="ans_3">Test3</label>
                     </li>
-                    <button type="submit">Submit</button>
+                    <button type="submit">BEVESTIG KEUZE</button>
                 </ul>
+
+                <?php
+                    $host = 'localhost';
+                    $user = 'root';
+                    $pass = '';
+                    $db   = 'wie_is_de_mol';
+
+                    $conn = new mysqli($host, $user, $pass, $db);
+
+                    if ($conn->connect_error) {
+                        die("Connectie mislukt: " . $conn->connect_error);
+                    }
+                ?>
             </form>
         </div>
+        
+        <h2>Kandidaat: <?php echo $_POST["code"];?></h2>
     </body>
 </html>
